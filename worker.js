cee9857ea5ca795f704fc8dcc3df5176ffc871cf
amirabbas1402 <129271190+amirabbas1402@users.noreply.github.com>
2026-05-26 13:42:12 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,8 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    
-    // Forward to your origin server
     const originURL = new URL(request.url);
     originURL.hostname = "103.83.86.246";
     
-    // Handle WebSocket upgrade
     const upgradeHeader = request.headers.get("Upgrade");
     if (upgradeHeader === "websocket") {
       return fetch(originURL.toString(), {
@@ -15,12 +11,9 @@ export default {
         body: request.body
       });
     }
-    
-    // Regular HTTP requests go to nginx cover page
     return fetch(originURL.toString(), {
       method: request.method,
-      headers: request.headers,
-      body: request.body
+      headers: request.headers
     });
   }
 }
