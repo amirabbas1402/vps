b563201a511018a152f9aec839c4a1b44a559146
amirabbas1402 <129271190+amirabbas1402@users.noreply.github.com>
2026-05-26 13:51:58 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,19 +1,21 @@
 export default {
   async fetch(request, env) {
-    const originURL = new URL(request.url);
-    originURL.hostname = "103.83.86.246";
-    
     const upgradeHeader = request.headers.get("Upgrade");
+    
     if (upgradeHeader === "websocket") {
-      return fetch(originURL.toString(), {
+      const url = new URL(request.url);
+      url.hostname = "103.83.86.246";
+      url.protocol = "https:";
+      
+      const response = await fetch(url.toString(), {
         method: request.method,
         headers: request.headers,
         body: request.body
       });
+      return response;
     }
-    return fetch(originURL.toString(), {
-      method: request.method,
-      headers: request.headers
-    });
+    
+    // Non-websocket requests
+    return new Response("OK", { status: 200 });
   }
 }
